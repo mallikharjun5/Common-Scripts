239159d7bff38875357bfd5433ff86f60851f2ba
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-11 15:16:48 +0530

    common scripts

--- common_script.sh
+++ common_script.sh
@@ -14,8 +14,8 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 SCRIPT_DIR=$PWD # for absoulute path
-MONGODB_HOST=mongodb.daws86s.fun
-MYSQL_HOST=mysql.daws86s.fun
+MONGODB_HOST=mongodb.mallikharjundevops.fun
+MYSQL_HOST=mysql.mallikharjundevops.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
